935a9b8896d511f13652ce4dd1537d5ab8202717
Kelechi Okoronkwo <kelechi.webdev@gmail.com>
2023-07-29 20:29:10 +0100

    Refactor : Mid Screen Responsiveness

--- src/pages/landing/Header.jsx
+++ src/pages/landing/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
 
   return (
     <header className="header bg h-screen">
-      <div className="bg-image hidden lg:block -z-10">
+      <div className="bg-image hidden md:block -z-10">
         <img src={polygon} alt="polygon" />
       </div>
       <div className="web-nav">
@@ -30,7 +30,7 @@ const Header = () => {
             <img src={logo} alt="" />
           </div>
 
-          <ul className="flex gap-10 lg:gap-20 flex-col md:flex-row  ">
+          <ul className="flex gap-10 md:gap-20 flex-col md:flex-row  ">
             <li className="text-3xl text-[#555555]">
               <a href="#">Projects</a>
             </li>
@@ -46,12 +46,12 @@ const Header = () => {
             <button className="">
               <a
                 href="#"
-                className="text-2xl lg:text-primaryColor font-600 text-[#222]"
+                className="text-2xl md:text-primaryColor font-600 text-[#222]"
               >
                 Sign in
               </a>
             </button>
-            <button className="lg:bg-green2  bg-green1 px-10 py-4 rounded-lg">
+            <button className="md:bg-green2  bg-green1 px-10 py-4 rounded-md">
               <a href="#" className="text-2xl text-white ">
                 Sign up
               </a>
@@ -68,7 +68,7 @@ const Header = () => {
 
           <div className="mobile-nav-icon">
             <nav className="nav container  mx-auto  py-10 flex justify-between items-center">
-              <div className="nav-cta flex items-center gap-10 lg:flex">
+              <div className="nav-cta flex items-center gap-10 md:flex">
                 <button onClick={handleClick} className="text-green2">
                   {isOpen ? <FaTimes size={25} /> : <FaBars size={25} />}
                 </button>
@@ -96,7 +96,7 @@ const Header = () => {
                 Sign in
               </a>
             </button>
-            <button className="lg:bg-green2  bg-green1 px-10 py-4 rounded-lg">
+            <button className="md:bg-green2  bg-green1 px-10 py-4 rounded-md">
               <a href="#" className="text-2xl text-white ">
                 Sign up
               </a>
@@ -105,28 +105,28 @@ const Header = () => {
         </div>
       </div>
 
-      <div className="hero  mx-auto mt-32 flex flex-col lg:flex-row justify-between items-center">
-        <div className="text-content w-full lg:w-[70%] text-center lg:text-left mb-16 lg:mb-0 fade-in-left">
+      <div className="hero mx-auto mt-32 flex flex-col md:flex-row justify-between items-center">
+        <div className="text-content w-full md:w-[70%] text-center md:text-left mb-16 md:mb-0 fade-in-left">
           <p className="text-primaryColor mb-7">
             <span className="text-4xl font-600">Welcome to Co_lab Apes, </span>
-            <span className="text-4xl font-light block lg:inline">
+            <span className="text-4xl font-light block md:inline">
               Let’s Remote Create !
             </span>
           </p>
-          <div className="w-[10%] bg-primaryColor h-2 rounded-full my-10 hidden lg:block"></div>
-          <h1 className="w-full lg:w-[70%] leading-normal text-[5rem] text-primaryColor font-semibold mb-12">
+          <div className="w-[10%] bg-primaryColor h-2 rounded-full my-10 hidden md:block"></div>
+          <h1 className="w-full leading-normal text-[5rem] text-primaryColor font-semibold mb-12">
             Unleash Your Creativity. Collaborate. Innovate.
           </h1>
           <p className="text-4xl text-acentColor">
             Join Co_lab Apes and Bring Your Ideas to Life!
           </p>
-          <div className="hero-cta flex flex-col-reverse lg:flex-row gap-20 justify-center lg:justify-normal  items-center mt-10">
-            <button className="bg-green2 px-10 py-7 rounded-lg">
+          <div className="hero-cta flex flex-col-reverse md:flex-row gap-20 justify-center md:justify-normal  items-center mt-10">
+            <button className="bg-green2 px-10 py-7 rounded-md">
               <a href="#" className="text-2xl text-white">
                 Get Started
               </a>
             </button>
-            <img className="hidden lg:block" src={next} alt="Emoji next" />
+            <img className="hidden md:block" src={next} alt="Emoji next" />
             <a
               className="text-3xl text-primaryColor border-b-2 border-primaryColor"
               href="#"
@@ -139,12 +139,12 @@ const Header = () => {
         <div className="image-content relative fade-in-top">
           <img src={frame1} alt="Frame" />
           <img
-            className="absolute top-24 -left-20 hidden lg:block"
+            className="absolute top-24 -left-20 hidden md:block"
             src={frame2}
             alt="frame"
           />
           <img
-            className="absolute -bottom-16 -right-7 hidden lg:block"
+            className="absolute -bottom-16 -right-7 hidden md:block"
             src={frame3}
             alt="frame"
           />
