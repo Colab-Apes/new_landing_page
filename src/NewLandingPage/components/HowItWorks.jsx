54b675b5d1376c8dbdc06fd1a8913635bf07f47a
Emmanuel Akinbo <akinbo.emmanuel@lmu.edu.ng>
2023-08-14 22:59:39 +0100

    added classnames

--- src/NewLandingPage/components/HowItWorks.jsx
+++ src/NewLandingPage/components/HowItWorks.jsx
@@ -12,8 +12,8 @@ const HowItWorks = () => {
         <div className="title text-center text-5xl text-[#333] font-bold mb-20">
           How It Works
         </div>
-        <div className="works flex flex-col md:flex-row gap-12 justify-between mb-10">
-          <div className="w-box rounded-[20px]  bg-[#ECF8EE] flex gap-10 p-7  w-full md:w-1/2 box-shadow">
+        <div className=" flex flex-col  md:flex-row gap-x-[20rem] justify-between items-start mb-10">
+          {/* <div className=" rounded-[20px]  bg-[#ECF8EE] flex gap-10 p-7  w-full md:w-1/2 box-shadow">
             <div className="w-number">
               <img src={one} alt="One" className="" />
             </div>
@@ -25,18 +25,34 @@ const HowItWorks = () => {
                 Sign Up and Complete your profile
               </p>
             </div>
+          </div> */}
+
+          <div className="flex gap-x-10 rounded-[20px]  shadow-[5px_10px_30px_0px_rgba(59,_128,_4,_0.10)] bg-[#ECF8EE]   py-10 px-10 w-full">
+            <div className="">
+              <img src={one} alt="One" className="" />
+            </div>
+            <div className=" flex flex-col mt-4 ">
+              <h1 className="text-[2.127rem] text-[#333] font-bold ">
+                Join Co_lab Apes
+              </h1>
+              <div className="border mt-8"></div>
+              <p className="text-[1.5rem] text-[#445C48]  font-bold mt-8">
+                Sign Up and Complete your profile
+              </p>
+            </div>
           </div>
-          <div className="w-box bg-[#ECF8EE] flex gap-10  p-7 w-full md:w-1/2 box-shadow  rounded-[20px]">
-            <div className="w-number">
+          {/* <div className="flex rounded-[20px]  bg-[#ECF8EE] h-[22rem] w-full"></div> */}
+
+          <div className="flex gap-x-20 rounded-[20px]  shadow-[5px_10px_30px_0px_rgba(59,_128,_4,_0.10)] bg-[#ECF8EE] py-10 px-10 w-full">
+            <div className="">
               <img src={two} alt="One" className="" />
             </div>
             <div className="w-text mt-7">
               <h1 className="text-[2rem] text-[#333333] font-semibold mb-16 ">
-                Explore Opportunities
+                Join Co_lab Apes
               </h1>
               <p className="text-[1.7rem] text-[#445C48] border-t pt-10">
-                Discover projects, connect with collaborators, and learn from
-                experts
+                Sign Up and Complete your profile
               </p>
             </div>
           </div>
