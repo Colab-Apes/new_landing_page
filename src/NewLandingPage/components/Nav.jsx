e9c6c4b999f0ca77b1db44ade49212bbb0723d7b
Emmanuel Akinbo <akinbo.emmanuel@lmu.edu.ng>
2023-08-14 11:38:35 +0100

    corrected the path

--- src/NewLandingPage/components/Nav.jsx
+++ src/NewLandingPage/components/Nav.jsx
@@ -1,7 +1,27 @@
 import React from "react";
-
+import logo from "../../assets/newimages/logo.png";
+import "../../App.css";
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <div>
+      <nav className="flex  bg-[#ECF8EE] font-lato items-center justify-between w-full px-12  py-4 mx-auto">
+        <img src={logo} alt="" />
+        <div className="flex items-center gap-x-10 text-[#555] font-bold text-[20px]">
+          <p>Projects</p>
+          <p>Learning pool</p>
+          <p>Create project</p>
+        </div>
+        <div className="flex gap-10">
+          <button className="px-[20px] py-[12px] text-[17px] text-white md:bg-green2 btngrad  bg-green1  rounded-[20px] font-bold">
+            Sign up
+          </button>
+          <button className="px-[8px] text-[#054E12] py-[12px] text-[17px]  font-bold">
+            Join waitlist
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Nav;
