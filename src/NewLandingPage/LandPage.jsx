a774b795573c49faedf4d1065f912008644e573d
Emmanuel Akinbo <akinbo.emmanuel@lmu.edu.ng>
2023-08-14 11:00:22 +0100

    working on nav

--- src/NewLandingPage/LandPage.jsx
+++ src/NewLandingPage/LandPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import logo from "../assets/newimages/logo.pooling";
 const LandPage = () => {
-  return <div>LandPage</div>;
+  return (
+    <div>
+      <nav className="flex items-center justify-center">
+        <img src={logo} alt="" />
+        div
+      </nav>
+    </div>
+  );
 };
 
 export default LandPage;
