24cf3e70714db0c17f6f852d1c3ba93467106ad6
Emmanuel Akinbo <akinbo.emmanuel@lmu.edu.ng>
2023-08-14 11:35:44 +0100

    almost done with nav

--- src/NewLandingPage/LandPage.jsx
+++ src/NewLandingPage/LandPage.jsx
@@ -4,7 +4,7 @@ import "../App.css";
 const LandPage = () => {
   return (
     <div>
-      <nav className="flex font-lato items-center justify-between container mx-auto">
+      <nav className="flex  bg-[#ECF8EE] font-lato items-center justify-between w-full px-12  py-4 mx-auto">
         <img src={logo} alt="" />
         <div className="flex items-center gap-x-10 text-[#555] font-bold text-[20px]">
           <p>Projects</p>
@@ -12,11 +12,11 @@ const LandPage = () => {
           <p>Create project</p>
         </div>
         <div className="flex gap-10">
-          <button className="px-[20px] py-[12px] text-[17px] rounded-[20px] font-bold font-600 text-[#222]">
-            Sign in
-          </button>
-          <button className=" text-[17px] text-white md:bg-green2 btngrad  bg-green1 px-12 py-5 rounded-[20px] font-bold">
+          <button className="px-[20px] py-[12px] text-[17px] text-white md:bg-green2 btngrad  bg-green1  rounded-[20px] font-bold">
             Sign up
+          </button>
+          <button className="px-[8px] text-[#054E12] py-[12px] text-[17px]  font-bold">
+            Join waitlist
           </button>
         </div>
       </nav>
