9480f6e1129bb14f5bc907a7dcf9161cc90db440
Emmanuel Akinbo <akinbo.emmanuel@lmu.edu.ng>
2023-08-17 14:00:41 +0100

    woked on md

--- src/NewLandingPage/components/Hero.jsx
+++ src/NewLandingPage/components/Hero.jsx
@@ -12,33 +12,33 @@ const Hero = () => {
         style={{
           backgroundImage: `url(${herobg})`,
         }}
-        className="pb-20 px-4  md:px-14 lg:px-20"
+        className="pb-20 px-4  md:px-10 lg:px-20"
       >
         {/* <div className="z-[-1] absolute w-[387px] h-[324.991px] bg-[#C26AAA] blur-[150px] rounded-[387px] left-[-30rem]"></div>
         <div className=" absolute z-[-2] w-[200px] h-[649.982px] bg-[#054E12] blur-[350px] rounded-full left-[-30rem]"></div> */}
 
-        <div className="hero xl:pt-10 w-full mx-auto flex flex-col md:flex-row justify-evenly md:justify-between items-center">
-          <div className="text-content w-full md:w-[70%] text-center md:text-left fade-in-left">
+        <div className="hero gap-x-20 xl:pt-10 w-full mx-auto flex flex-col md:flex-row justify-evenly md:justify-between items-center">
+          <div className=" w-full md:w-[70%]  text-center md:text-left fade-in-left">
             <p className="text-primaryColor mb-7">
-              <span className="text-[2rem]  font-bold hidden lg:block text-[#3B513F]">
+              <span className=" md:text-[1.58rem] lg:text-[2rem]  font-bold hidden md:block text-[#3B513F]">
                 Welcome to Co_lab Apes,
                 <span className="font-normal"> Let’s Remote Create ! </span>
               </span>
               {/* <span className="text-4xl font-light hidden md:inline"></span> */}
             </p>
-            <div className="w-[15%] bg-primaryColor h-2 rounded-full  mt-12 hidden lg:block"></div>
-            <h1 className="w-full text-[3.6rem] text-center lg:text-left xl:text-[3.8rem] leading-[1.8]  mt-14 text-[#054E12] font-[750] xl:font-[650] ">
+            <div className="w-[15%] bg-primaryColor h-2 rounded-full mt-8 lg:mt-12 hidden md:block"></div>
+            <h1 className="w-full text-[3.6rem] text-center md:text-left md:text-[3rem]  lg::text-[3.8rem] leading-[1.8]  mt-14 text-[#054E12] font-[750] xl:font-[650] ">
               Unleash Your Creativity. <br /> Collaborate. Innovate.
             </h1>
-            <p className="text-3xl md:text-2xl xl:text-[1.8rem] mt-14 text-[#3B513F]">
+            <p className="text-3xl md:text-2xl xl:text-[1.8rem] md:mt-10 lg:mt-14 text-[#3B513F]">
               Join Co_lab Apes and Bring Your Ideas to Life!
             </p>
-            <div className="hero-cta hidden md:flex flex-col-reverse md:flex-row gap-12 justify-center md:justify-normal  items-center mt-5 xl:mt-12">
-              <button className="btngrad font-bold shadow-[10px_10px_30px_0px_rgba(59,_128,_4,_0.10)] px-16 py-8 rounded-[18px] text-[16px] text-white">
+            <div className="hidden md:flex flex-col-reverse md:flex-row  lg:gap-12 justify-center md:justify-between  items-center md:mt-10  lg:mt-5 xl:mt-12">
+              <button className="btngrad font-bold shadow-[10px_10px_30px_0px_rgba(59,_128,_4,_0.10)] lg:px-16  md:px-10 md:py-6 lg:py-8 rounded-[18px] text-[12px] lg:text-[16px] text-white ">
                 Get Started
               </button>
               <img
-                className="hidden md:block  object-contain w-[8rem] xl:w-[6.9rem]"
+                className="hidden md:block  object-contain lg:w-[8rem] md:w-[6rem] xl:w-[6.9rem]"
                 src={emoji}
                 alt="Emoji next"
               />
@@ -52,20 +52,20 @@ const Hero = () => {
           </div>
 
           <div className="image-content relative fade-in-top  ">
-            <img src={frame1} alt="Frame" />
+            <img src={frame1} alt="Frame" className="" />
             <img
-              className="absolute top-24 -left-20 hidden md:block"
+              className="absolute object-contain md:w-[14rem] lg:w-full top-24 md:-left-14 lg:-left-20 hidden md:block"
               src={frame2}
               alt="frame"
             />
             <img
-              className="absolute -bottom-16 -right-7 hidden md:block"
+              className="absolute object-contain  md:w-[14rem] lg:w-full  -bottom-16 md:-bottom-0 md:-right-2  -right-7 hidden md:block"
               src={frame3}
               alt="frame"
             />
           </div>
 
-          <div className="hero-cta hc-mobile container mx-auto  lg:hidden">
+          <div className="hero-cta hc-mobile container mx-auto md:hidden">
             <button className="w-full  btngrad px-10 py-7 rounded-xl mb-10">
               <a href="#" className="text-2xl text-white">
                 Get Started
