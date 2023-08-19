64c75baec4c937354e042a8289898f5bbe1f2aa4
Emmanuel Akinbo <akinbo.emmanuel@lmu.edu.ng>
2023-08-19 10:56:51 +0100

    just worked on one of them

--- src/NewLandingPage/components/Faqs.jsx
+++ src/NewLandingPage/components/Faqs.jsx
@@ -5,15 +5,16 @@ import plus from "../../assets/icons/plus.svg";
 
 const Faqs = () => {
   const [arr, setarr] = useState([]);
+  const [openclose, setopenclose] = useState(false);
   return (
-    <div className="font-lato px-4  md:px-14 lg:px-20 mt-64 lg:mt-36 relative">
+    <div className="font-lato px-4 p  md:px-14 lg:px-20 mt-64 lg:mt-36 relative">
       <div className="title mt-36 text-center  text-5xl lg:text-6xl ext-[#333] lg:text-[#3B8004] font-bold mb-20">
         Frequently Asked Questions
       </div>
       <div className="absolute z-[-1]  hidden lg:block blur-[200px] bottom-[-20rem] left-[-20rem] w-[387px] h-[410px] bg-[#C26AAA] rounded-[400px]"></div>
       <div className="absolute z-[-1]   hidden lg:block blur-[200px] top-[-30rem] right-[0rem] w-[200px] h-[200px] bg-[#C26AAA] rounded-[410.85px]"></div>
 
-      <div className="faq bg-[#D5EEDA] mt-2 w-full text-center  mx-auto px-10 relative rounded-[20px] h-[450px] py-10">
+      <div className="faq  bg-[#D5EEDA]  pb-[10rem] mt-2 w-full text-center  mx-auto px-10 relative lg:mt-40 rounded-[20px] min-h-[450px] py-10">
         <div className="w-full flex justify-center items-center">
           <div className="bg-white w-[50rem] flex rounded-[20px]  justify-between px-2 py-2">
             <button className="btngrad text-white text-2xl font-bold rounded-[15px] py-6 w-[10rem] lg:py-[16px] lg:w-[14rem] ">
@@ -29,14 +30,35 @@ const Faqs = () => {
           </div>
         </div>
 
-        <div className=" lg:px-32 mt-20">
-          <div className="flex justify-between items-center border-b border-b-[#999] pb-10">
-            <p className="text-3xl font-bold  text-[#054E12]">
-              What is Co_lab Apes?
+        <div className=" lg:px-32 mt-20 0">
+          <div className="border-b border-b-[#999] pb-10">
+            <div
+              onClick={() => {
+                setopenclose(!openclose);
+              }}
+              className="flex cursor-pointer justify-between mb-4 items-center "
+            >
+              <p className="text-3xl font-bold  text-[#054E12]">
+                What is Co_lab Apes?
+              </p>
+              <img src={plus} alt="" className="cursor-pointer" />
+            </div>{" "}
+            <p
+              className={
+                openclose
+                  ? "text-[#054E12] text-3xl text-left mb-8 leading-[3.5rem]"
+                  : "hidden"
+              }
+            >
+              Colab apes aim to link learning and earning by encouraging people
+              to develop their skills on the job and creating real-life projects
+              that generate revenues, then distributing the profits to all team
+              members. Stating its financial targets and discerning how the
+              business plans to achieve them promptly.
             </p>
-            <img src={plus} alt="" className="cursor-pointer" />
           </div>
-          <div className="flex justify-between mt-20 items-center border-b border-b-[#999] pb-10">
+
+          <div className="flex justify-between mt-20  items-center border-b border-b-[#999] pb-10">
             <p className="text-3xl font-bold  text-[#054E12]">
               Is Co_lab Apes free to join?
             </p>
