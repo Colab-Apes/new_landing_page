659826fbc2e21dc5d4ad26b9ef551c1789963543
Emmanuel Akinbo <akinbo.emmanuel@lmu.edu.ng>
2023-08-22 14:08:51 +0100

    done

--- src/NewLandingPage/components/Faqs/faqsdata.js
+++ src/NewLandingPage/components/Faqs/faqsdata.js
@@ -2,17 +2,17 @@ export const faqscolabapes = [
   {
     title: "What is Co_lab Apes?",
     content:
-      "Colab apes aim to link learning and earning by encouraging people to develop their skills on the job and creating real-life projects that generate revenues, then distributing the profits to all team members. Stating its financial targets and discerning how the business plans to achieve them promptly.",
+      "Co_lab Apes platform is an innovative online community that empowers collaboration and innovation among creators and investors. It offers a range of products, including a social media hub, a learning pool for live online group-based learning, a build feature for collaborative project creation, and a value stake protocol for early-stage fundraising.",
   },
   {
-    title: "Is Co_lab Apes free to join?",
+    title: "How does Co_lab Apes work?",
     content:
-      "Colab apes aim to link learning and earning by encouraging people to develop their skills on the job and creating real-life projects that generate revenues, then distributing the profits to all team members. Stating its financial targets and discerning how the business plans to achieve them promptly.",
+      "Co_lab Apes works by bringing together creators and investors in a virtual environment where they can connect, collaborate on projects, and share knowledge. Users can join the platform, create profiles, explore projects, and collaborate with like-minded individuals to bring their ideas to life.",
   },
   {
-    title: "Why should I join Co_lab Apes?",
+    title: "Is Co_lab Apes a free platform to use?",
     content:
-      "Colab apes aim to link learning and earning by encouraging people to develop their skills on the job and creating real-life projects that generate revenues, then distributing the profits to all team members. Stating its financial targets and discerning how the business plans to achieve them promptly.",
+      "Yes, Co_lab Apes is a free platform to use. Users can join and create accounts without any subscription fees or charges.",
   },
 ];
 
@@ -38,27 +38,28 @@ export const faqsseeall = [
   {
     title: "What is Co_lab Apes?",
     content:
-      "Colab apes aim to link learning and earning by encouraging people to develop their skills on the job and creating real-life projects that generate revenues, then distributing the profits to all team members. Stating its financial targets and discerning how the business plans to achieve them promptly.",
+      "Co_lab Apes platform is an innovative online community that empowers collaboration and innovation among creators and investors. It offers a range of products, including a social media hub, a learning pool for live online group-based learning, a build feature for collaborative project creation, and a value stake protocol for early-stage fundraising",
   },
   {
-    title: "Is Co_lab Apes free to join?",
+    title: "How does Co_lab Apes work?",
     content:
-      "Colab apes aim to link learning and earning by encouraging people to develop their skills on the job and creating real-life projects that generate revenues, then distributing the profits to all team members. Stating its financial targets and discerning how the business plans to achieve them promptly.",
+      "Co_lab Apes works by bringing together creators and investors in a virtual environment where they can connect, collaborate on projects, and share knowledge Users can join the platform, create profiles, explore projects, and collaborate with like-minded individuals to bring their ideas to life.",
   },
   {
-    title: "Why should I join Co_lab Apes?",
+    title: "Is Co_lab Apes a free platform to use?",
     content:
-      "Colab apes aim to link learning and earning by encouraging people to develop their skills on the job and creating real-life projects that generate revenues, then distributing the profits to all team members. Stating its financial targets and discerning how the business plans to achieve them promptly.",
+      "Yes, Co_lab Apes is a free platform to use. Users can join and create accounts without any subscription fees or charges.",
   },
   {
-    title: "Is Co_lab Apes free of charge?",
+    title:
+      "How does Co_lab Apes ensure the security and privacy of my project data?",
     content:
-      "Colab apes aim to link learning and earning by encouraging people to develop their skills on the job and creating real-life projects that generate revenues, then distributing the profits to all team members. Stating its financial targets and discerning how the business plans to achieve them promptly.",
+      "Co_lab Apes takes data security and privacy seriously. The platform implements robust security measures to safeguard project data and ensure user privacy.",
   },
   {
-    title: "How can I invite members, share and communicate with them?",
+    title: "Can I integrate other tools or services with Co_lab Apes",
     content:
-      "Colab apes aim to link learning and earning by encouraging people to develop their skills on the job and creating real-life projects that generate revenues, then distributing the profits to all team members. Stating its financial targets and discerning how the business plans to achieve them promptly.",
+      "Co_lab Apes is designed to be flexible and supports integrations with other tools or services that enhance the collaborative experience for users",
   },
   {
     title: "Does Co_lab Apes have customer support?",
