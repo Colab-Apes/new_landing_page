c1035901d91bcbdfe93525c15a30a0de3344a839
Emmanuel Akinbo <akinbo.emmanuel@lmu.edu.ng>
2023-08-30 12:06:54 +0100

    added image

--- src/NewLandingPage/components/WaitList.jsx
+++ src/NewLandingPage/components/WaitList.jsx
@@ -12,7 +12,7 @@ const WaitList = ({ openjoinwaitlistmodal, setopenjoinwaitlistmodal }) => {
     >
       <div
         className={
-          " rounded-[20px] lg:rounded-[50px] bg-[url('./assets/waitlist.png')]  z-[120] bg-[#F5FFF7] flex items-center justify-between min-h-[63rem] w-full py-0  sm:pb-0 md:py-4  sm:min-h-[35rem] lg:min-h-[32rem] lg:w-full  xl:min-h-[54rem] xl:h-[] lg:p-[3rem]  xl:w-full  md:w-ful sm:px-8 md:px-[3rem] xl:px-[3.5rem] opacity-100 xl:py-[3.5rem]  relative duration-[800ms] ease-in-out"
+          " rounded-[20px] lg:rounded-[50px] bg-[url('./assets/waitlist1.png')]  z-[120] bg-[#F5FFF7] flex items-center justify-between min-h-[63rem] w-full py-0  sm:pb-0 md:py-4  sm:min-h-[35rem] lg:min-h-[32rem] lg:w-full  xl:min-h-[54rem] xl:h-[] lg:p-[3rem]  xl:w-full  md:w-ful sm:px-8 md:px-[3rem] xl:px-[3.5rem] opacity-100 xl:py-[3.5rem]  relative duration-[800ms] ease-in-out"
         }
       >
         <TfiClose
