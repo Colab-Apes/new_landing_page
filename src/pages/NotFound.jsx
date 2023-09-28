9927b4615bec39299f8554d298aa91b6840784fd
Emmanuel Akinbo <akinbo.emmanuel@lmu.edu.ng>
2023-09-28 08:53:51 +0100

    worked on notfound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import logo from "./assets/logo404.png";
 import zero from "./assets/zero.png";
+import plus from "./assets/plus.svg";
+import plus1 from "./assets/plugreen.svg";
+import circlegreen from "./assets/circlegreen.svg";
+import circlewhite from "./assets/circle.svg";
 const NotFound = () => {
   return (
     <div>
@@ -8,8 +12,59 @@ const NotFound = () => {
         <div className="absolute top-0 left-0 ">
           <img src={logo} alt="" className="w-[20vw] object-contain" />{" "}
         </div>
+        {/* <img src={circle} alt="" className="absolute top-3 w-[2rem] object-contain"/> */}
+        <img
+          src={circlegreen}
+          alt=""
+          className="absolute top-[40rem] left-[100rem] w-[1.2rem] object-contain "
+        />
+        <img
+          src={circlegreen}
+          alt=""
+          className="absolute top-[20rem] left-[40rem] z-20 w-[1.2rem] object-contain "
+        />
+        <img
+          src={circlewhite}
+          alt=""
+          className="absolute top-[10rem] left-[82rem] w-[1.2rem] object-contain "
+        />
+        <img
+          src={circlewhite}
+          alt=""
+          className="absolute top-[40rem] left-[80.2rem] w-[1.2rem] object-contain "
+        />
+        <img
+          src={circlewhite}
+          alt=""
+          className="absolute top-[40rem] left-[80.2rem] w-[1.2rem] object-contain "
+        />
+        <img
+          src={circlewhite}
+          alt=""
+          className="absolute top-[18rem] left-[89rem] w-[1.2rem] object-contain "
+        />
+        <img
+          src={circlewhite}
+          alt=""
+          className="absolute bottom-[2rem] left-[60rem] w-[1.2rem] object-contain "
+        />
+        <img
+          src={plus}
+          alt=""
+          className="absolute top-[5rem] left-[60rem] w-[2rem] object-contain"
+        />
+        <img
+          src={plus}
+          alt=""
+          className="absolute top-[5rem] left-[60rem] w-[2rem] object-contain"
+        />
+        <img
+          src={plus1}
+          alt=""
+          className="absolute w-[2rem] bottom-[10rem]  left-[40rem]"
+        />
         <div className="absolute top-20 left-32">
-          <div className="bg-[#c26AAA] absolute translate-x-[-50%] translate-y-[50%] top-[-20%] blur-[100px] left-[66%]  rounded-full  w-[200px] h-[200px]"></div>
+          <div className="bg-[#c26AAA] absolute translate-x-[-50%] translate-y-[50%] top-[-20%] blur-[90px] left-[66%]  rounded-full  w-[200px] h-[200px]"></div>
           <div className="h-[400px] w-[400px] rounded-full bg-[#054E12] blur-[200px]"></div>
         </div>
         <div className="h-[75vh] flex relative flex-col items-center justify-center w-[75vh] rounded-full bg-[#D9D9D91A]/[10%]">
